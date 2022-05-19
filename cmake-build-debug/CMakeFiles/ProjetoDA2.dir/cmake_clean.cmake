6e2c0ef76f33a7d69f6cdccb6cd276454dd0a821
André Ávila <avila_andre14@hotmail.com>
2022-05-20 00:04:07 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ProjetoDA2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetoDA2.dir/main.cpp.obj"
+  "ProjetoDA2.exe"
+  "ProjetoDA2.exe.manifest"
+  "ProjetoDA2.pdb"
+  "libProjetoDA2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjetoDA2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ProjetoDA2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ProjetoDA2.
+# This may be replaced when dependencies are built.
